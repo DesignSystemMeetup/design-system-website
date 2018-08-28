ea572b55afa3dab9bcc82d513027b5134b3c251c
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2018-08-28 16:03:05 +1000

    added edit this page link to footer
    Signed-off-by: Dominik Wilkowski <Hi@Dominik-Wilkowski.com>

--- code/layout/footer.js
+++ code/layout/footer.js
@@ -7,19 +7,26 @@ import React, { Fragment } from 'react';
  *
  * @disable-docs
  */
-const Partial = ({ _body }) => (
+const Footer = ({ _body, _ID, _relativeURL }) => (
 	<footer className="wrapper">
 		{ _body }
+		<a className="iconText" href={`https://github.com/DesignSystemMeetup/design-system-website/blob/master/content/${ _ID }`}>
+			<span className="iconText-text">Edit this page</span>
+
+			<svg className="iconText-icon" role="img" title="Logo of GitHub">
+				<use xlinkHref={ _relativeURL( 'assets/svg/sprite.svg#github', _ID ) }/>
+			</svg>
+		</a>
 	</footer>
 );
 
-Partial.propTypes = {
+Footer.propTypes = {
 	/**
 	 * _body: (test)(12)
 	 */
 	_body: PropTypes.node.isRequired,
 };
 
-Partial.defaultProps = {};
+Footer.defaultProps = {};
 
-export default Partial;
+export default Footer;
--- content/_shared/footer.md
+++ content/_shared/footer.md
@@ -3,5 +3,4 @@ layout: footer
 ---
 
 Logo design by [Jeniffer Heng](https://dribbble.com/hengtime), website by [Thinkmill](https://www.thinkmill.com.au),
-[edit these pages](https://github.com/DesignSystemMeetup/design-system-website),
 [Code of Conduct](https://designsystemmeetup.com/code-of-conduct/)
