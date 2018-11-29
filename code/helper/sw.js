e5ecedf830e523b76e032a58aa3b026994d75388
Nathan Simpson <hello@nathansimpson.design>
2018-11-30 09:44:43 +1100

    Made reading directories syncronous

--- code/helper/sw.js
+++ code/helper/sw.js
@@ -1,33 +1,33 @@
 const fs = require("fs");
+const path = require("path");
 const PKG = require("../../package.json");
 const NAME = `"design-system-meetup-${PKG.version}"`;
 
 let filesToCache = ['"/"']
 
 function read(dir) {
-	fs.readdir(dir, function(err, children) {
-		if(children){
-			children = children.filter(word => word !== 'CNAME');
-			children.forEach(child => read(`${dir}/${child}`));
-		}else{
-			let item = dir.replace('docs', '').replace('/index.html', '');
+	fs.readdirSync(dir).forEach(child => {
+		// if child is a file
+		if(path.extname(child)){
+			let item = `${dir.replace('docs', '')}/${child.replace('/index.html', '')}`;
 			if(item !== ''){filesToCache.push(`"${item}"`)}
+		//otherwise it's a folder
+		}else if(!child.includes('CNAME')){
+			read(`${dir}/${child}`);
 		}
-	});
+	})
 }
 
 read('docs');
 
 fs.readFile('code/assets/js/sw.template.js', 'utf8', function(err, template) {
-	console.log('Service worker will cache these files...', filesToCache);
-	let data = template;
-	data = data.replace(/\$name/g, NAME);
-	data = data.replace(/\$filesToCache/g, filesToCache);
-	data = data.replace(/^\s*[\r\n]/gm, '');
+	//console.log('Service worker will cache these files...', filesToCache);
+	let file = template
+		.replace(/\$name/g, NAME)
+		.replace(/\$filesToCache/g, filesToCache)
+		.replace(/^\s*[\r\n]/gm, '');
 
-	fs.writeFile("docs/assets/js/sw.min.js", data, function(err) {
-		if (err) {
-			return console.log(err);
-		}
+	fs.writeFile("docs/assets/js/sw.min.js", file, function(err) {
+		if (err) {return console.log(err)}
 	});
 });
